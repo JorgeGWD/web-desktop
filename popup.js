e78cae25dab9adc8943c3b3c1a392db79e8ab00c
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-06 14:59:29 -0500

    Extension initialization

---
+++ popup.js
@@ -0,0 +1,6 @@
+// popup.js
+function abrirNuevaPestana() {
+  chrome.tabs.create({ url: 'index.html' });
+}
+
+document.getElementById('abrirPestanaBtn').addEventListener('click', abrirNuevaPestana);
