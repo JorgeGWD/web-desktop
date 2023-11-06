bf0a995bc6bac8bcd9cad1a302058caa6adbc5f9
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-06 16:25:21 -0500

    Adjusts body and clock style

--- clock.js
+++ clock.js
@@ -1,14 +1,14 @@
 function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
 
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    var timeString = hours + ':' + minutes + ':' + seconds;
+    let timeString = hours + ':' + minutes + ':' + seconds;
 
     document.getElementById('clock').innerText = timeString;
 }
