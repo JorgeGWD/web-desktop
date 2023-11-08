2b24cba7170d39d16abc7bf4e989ec8340e56cd1
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-08 16:20:35 -0500

    Adjust manifest and background file to open new tab with only clicking on icon on extension tab

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-function updateBackground() {
-    let today = new Date();
-    let dayOfYear = getDayOfYear(today);
-    let collectionId = '206';
-    let imageUrl = `https://source.unsplash.com/collection/${collectionId}/${dayOfYear} 1920x1080`;
-
-    document.body.style.backgroundImage = `url('${imageUrl}')`;
-}
-  
-function getDayOfYear(date) {
-    let start = new Date(date.getFullYear(), 0, 0);
-    let diff = date - start;
-    let oneDay = 1000 * 60 * 60 * 24;
-    return Math.floor(diff / oneDay);
-}
-
-updateBackground();
-  
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({ url: 'index.html' });
+});
