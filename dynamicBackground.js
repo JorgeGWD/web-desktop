6509c425b02d8f471e90e42bde0944a7de5daca0
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-08 16:23:24 -0500

    Add new dynamic background file to change index background dynamically

---
+++ dynamicBackground.js
@@ -0,0 +1,17 @@
+function updateBackground() {
+    let today = new Date();
+    let dayOfYear = getDayOfYear(today);
+    let collectionId = '206';
+    let imageUrl = `https://source.unsplash.com/collection/${collectionId}/${dayOfYear} 1920x1080`;
+
+    document.body.style.backgroundImage = `url('${imageUrl}')`;
+}
+  
+function getDayOfYear(date) {
+    let start = new Date(date.getFullYear(), 0, 0);
+    let diff = date - start;
+    let oneDay = 1000 * 60 * 60 * 24;
+    return Math.floor(diff / oneDay);
+}
+
+updateBackground();
