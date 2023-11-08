567ed3bfaeff3b95a97c75a24259d8f270cca9f8
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-08 13:31:52 -0500

    Todo file refactor

--- todo.js
+++ todo.js
@@ -1,39 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Elementos del DOM
     const taskInput = document.getElementById("taskInput");
     const addTaskBtn = document.getElementById("addTaskBtn");
     const deleteCompletedBtn = document.getElementById("deleteCompletedBtn");
     const taskList = document.getElementById("taskList");
 
-    // Evento para agregar una tarea
     addTaskBtn.addEventListener("click", function() {
       const taskText = taskInput.value.trim();
 
       if (taskText !== "") {
-        // Crear elemento de lista
-        const li = document.createElement("li");
-        li.textContent = taskText;
+            const li = document.createElement("li");
+            li.textContent = taskText;
 
-        // Añadir evento para marcar como completada
-        li.addEventListener("click", function() {
-          li.classList.toggle("completed");
-        });
+            li.addEventListener("click", function() {
+            li.classList.toggle("completed");
+            });
 
-        // Añadir elemento de lista a la lista de tareas
-        taskList.appendChild(li);
+            taskList.appendChild(li);
 
-        // Limpiar el campo de entrada
-        taskInput.value = "";
-      }
+            taskInput.value = "";
+        }
     });
 
-    // Evento para eliminar tareas completadas
     deleteCompletedBtn.addEventListener("click", function() {
-      const completedTasks = document.querySelectorAll(".completed");
+        const completedTasks = document.querySelectorAll(".completed");
 
-      completedTasks.forEach(function(task) {
-        task.remove();
-      });
+        completedTasks.forEach(function(task) {
+            task.remove();
+        });
     });
 });
 
